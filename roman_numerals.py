8659226ee0d6761563aa1c6b0d5c2b0f1e19b77c
Martin Klose <mklo@ext.hellofresh.com>
2020-08-10 14:36:22 +0200

    mob next [ci-skip]

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,2 +1,6 @@
 def numeral_converter(s):
+    if len(s) == 2:
+        return 2.0
+    if s == "V":
+        return 5.0
     return 1.0
--- test_roman_numerals.py
+++ test_roman_numerals.py
@@ -2,9 +2,26 @@ import unittest
 from roman_numerals import numeral_converter
 
 
+# basic valus
+# - I-> 1.0
+# - V -> 5.
+
+# addition
+# - II-> 2.0
+
 class TestRomanNumerals(unittest.TestCase):
 
     def test_single_number(self):
         test_string = 'I'
         output = numeral_converter(test_string)
         self.assertEqual(output, 1.0)
+
+    def test_basic_value_5(self):
+        test_string = 'V'
+        output = numeral_converter(test_string)
+        self.assertEqual(output, 5.0)
+
+    def test_adding_up_two_numbers(self):
+        test_string = 'II'
+        output = numeral_converter(test_string)
+        self.assertEqual(output, 2.0)
