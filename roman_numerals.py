21f338f6265558a4cb03a9ae0c96129b6431d014
TomKealy <tom.kealy.tk@gmail.com>
2020-08-10 14:43:03 +0200

    mob next [ci-skip]

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,6 +1,12 @@
+string_to_numbers = {
+    'I': 1.0,
+    'V': 5.0
+}
+
+
 def numeral_converter(s):
     if len(s) == 2:
         return 2.0
     if s == "V":
-        return 5.0
+         return 5.0
     return 1.0
