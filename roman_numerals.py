7346ec74c27cc7c79316e2abb22920a6e5c094bf
TomKealy <tom.kealy.tk@gmail.com>
2020-08-19 12:48:30 +0200

    mob next [ci-skip]

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,12 +1,20 @@
 string_to_numbers = {
     'I': 1.0,
-    'V': 5.0
+    'V': 5.0,
+    'X': 10.0,
+    'L': 50.0,
+    'C': 100.0,
+    'D': 500.0,
+    'M': 1000.0
 }
 
 
 def numeral_converter(s):
+    """
+    Function to convert roman numerals to floats
+    :param s: string
+    :return: flat
+    """
     if len(s) == 2:
         return 2.0
-    if s == "V":
-         return 5.0
-    return 1.0
+    return string_to_numbers[s]
--- test_roman_numerals.py
+++ test_roman_numerals.py
@@ -5,6 +5,12 @@ from roman_numerals import numeral_converter
 # basic valus
 # - I-> 1.0
 # - V -> 5.
+# - X -> 10,
+# - L -> 50
+# - C -> 100
+# - D -> 500
+# - M -> 1000
+
 
 # addition
 # - II-> 2.0
@@ -25,3 +31,14 @@ class TestRomanNumerals(unittest.TestCase):
         test_string = 'II'
         output = numeral_converter(test_string)
         self.assertEqual(output, 2.0)
+
+    def test_adding_up_two_numbers_XX(self):
+        test_string = 'VV'
+        output = numeral_converter(test_string)
+        self.assertEqual(output, 6.0)
+
+    def test_test_all_combination_of_numbers(self):
+        # acceptance test
+        test_string = 'MMCDLXVII'
+        output = numeral_converter(test_string)
+        self.assertEqual(output, 2467.0)
